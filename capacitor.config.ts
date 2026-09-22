19e5b54e31e35b29d434dd3ddffaf51852823edb
toxicomediante <yoseba@gmail.com>
2026-09-22 17:23:21 +0200

    Configure Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.yosesproject.dummy',
+  appName: 'Dummy',
+  webDir: 'dist'
+}
+
+export default config
